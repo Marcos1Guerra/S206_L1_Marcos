25e676989bbd31f47dd6ab826d4adc7ab543309d
Marcos1Guerra <marcos.guerra@ges.intel.br>
2023-11-04 10:07:56 -0300

    relatorio 04 - aula

--- testes/cypress/support/commands.js
+++ testes/cypress/support/commands.js
@@ -23,3 +23,9 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login',(user,senha) => {
+    cy.get('#username').type(user)
+    cy.get('#password').type(senha)
+    cy.get('.btn-primary').click()
+})
